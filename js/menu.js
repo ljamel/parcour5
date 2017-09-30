3619f343e5e7912bfd30927b54b029b603a33bd7
pubs1896 <djamallamri@yahoo.fr>
2017-10-01 01:34:59 +0200

    premier commit

---
+++ js/menu.js
@@ -0,0 +1,37 @@
+function Menu() {
+
+    this.scroll = function () {
+
+        if (window.screen.width > 700) {
+            if (window.scrollY > 50) {
+                document.getElementById('menu-header').style.display = "none";
+                document.getElementById('menu-header-scrollBas').style.display = "block";
+            }
+            if (window.scrollY < 50) {
+                document.getElementById('menu-header').style.display = "block";
+                document.getElementById('menu-header-scrollBas').style.display = "none";
+            }
+        }
+
+    };
+
+    this.mobile = function() {
+            document.getElementById('deroul-menu').style.display = "block";
+            document.getElementById('menu-mobile').style.display = "none";
+            document.getElementById('menu-close').style.display = "block";
+    };
+
+    this.closeMenu = function () {
+        document.getElementById('deroul-menu').style.display = "none";
+        document.getElementById('menu-mobile').style.display = "block";
+        document.getElementById('menu-close').style.display = "none";
+    };
+
+}
+var menuAccueil = new Menu();
+
+window.addEventListener('scroll', menuAccueil.scroll, false);
+document.getElementById('menu-mobile').addEventListener('click', menuAccueil.mobile, false);
+document.getElementById('menu-close').addEventListener('click', menuAccueil.closeMenu, false);
+
+
---
+++ js/slide.js
@@ -0,0 +1,28 @@
+function Slide() {
+    this.nbimage = '';
+
+    this.image = function() {
+        if (this.nbimage != 'a') {
+            document.getElementById('img-slide2').style.display = 'flex';
+            document.getElementById('img-slide1').style.display = 'none';
+            this.nbimage = 'a';
+
+        } else {
+            document.getElementById('img-slide2').style.display = 'none';
+            document.getElementById('img-slide1').style.display = 'flex';
+            this.nbimage = 'b';
+
+        }
+    };
+
+    this.autoplay = function() {
+        setInterval(cursor.image, 18500);
+
+    }
+}
+
+var cursor = new Slide();
+var play = new Slide();
+play.autoplay();
+document.getElementById('bouton-slide-right').addEventListener('click', cursor.image, false);
+document.getElementById('bouton-slide-left').addEventListener('click', cursor.image, false);
