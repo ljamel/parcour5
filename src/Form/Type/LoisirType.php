7a9c4f9d261bed291cc1ba05f7cb9fe04d60dc01
pubs1896 <djamallamri@yahoo.fr>
2017-12-04 13:18:52 +0100

    Ajout du backend et commentaire

---
+++ src/Form/Type/LoisirType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MicroCMS\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('content', TextareaType::class);
+    }
+
+    public function getName()
+    {
+        return 'comment';
+    }
+}
