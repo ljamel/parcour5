1c326f5c98295844c6dccb9cc6b90231da764ccc
pubs1896 <djamallamri@yahoo.fr>
2017-12-09 22:34:34 +0100

    Test et corréction pour sensiolabs

--- web/index.php
+++ web/index.php
@@ -7,7 +7,6 @@
  */
 
 
-
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
@@ -15,6 +14,5 @@ $app = new Silex\Application();
 require __DIR__.'/../app/config/dev.php';
 require __DIR__.'/../app/app.php';
 require __DIR__.'/../app/routes.php';
-
 
 $app->run();
