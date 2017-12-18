1ea8ec71bc9278b0f2de9fa8975c1ffd8805598c
pubs1896 <djamallamri@yahoo.fr>
2017-12-18 14:14:25 +0100

    Ajout dans la barre de recherche le champ budget et modification de la base de donnée

---
+++ src/Form/Type/LoisirTypeAdd.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace MicroCMS\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class LoisirType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+        $builder
+            ->add('etat', ChoiceType::class, array(
+                'choices' => array('0' => 'En attente', '1' => 'Publier', '3' => 'Refuser' ) ))
+            ->add('title', TextType::class)
+            ->add('lien', TextType::class)
+            ->add('position', TextType::class)
+            ->add('image', TextType::class)
+
+            ->add('content', TextareaType::class);
+    }
+
+    public function getName()
+    {
+        return 'loisir';
+    }
+}
+
+class LoisirTypeAdd extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+        $builder
+            ->add('etat', ChoiceType::class, array(
+                'choices' => array('0' => 'En attente', '1' => 'Publier', '3' => 'Refuser' ) ))
+            ->add('title', TextType::class)
+            ->add('lien', TextType::class)
+            ->add('position', TextType::class)
+            ->add('image', FileType::class)
+
+            ->add('content', TextareaType::class);
+    }
+
+    public function getName()
+    {
+        return 'loisir';
+    }
+}
---
+++ web/js/map-result.js
@@ -0,0 +1,82 @@
+function initMap() {
+    var paris1 = {
+        lat: 48.866667,
+        lng: 2.333333
+    };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: paris1
+    });
+
+    // Modifi le cluster
+    var image = 'http://localhost/parcour-5/web/images/cycles.png';
+    var image1 = '../images/Calque0.png';
+    ajaxGet("http://parcour-5/clustersAll/", function (reponse) {
+
+        // Transforme la rÃ©ponse en un tableau d'adresses
+        var objects = JSON.parse(reponse);
+        objects.forEach(function (object) {
+
+            // Les markers individuel ne sont afficher qu'aprÃ¨s l'Ã©vÃ©nement du changement de zoom.
+            google.maps.event.addListener(map, 'zoom_changed', function () {
+                console.log(map.zoom);
+                if (map.zoom > 12) {
+                    // position pour cluster individuel
+                    var paris = {
+                        lat: object.position.lat,
+                        lng: object.position.lng
+                    }
+                }
+
+                    var marker = new google.maps.Marker({
+                        position: paris,
+                        icon: image,
+                        map: map,
+                        title: 'Location vÃ©lib',
+                        zIndex: 2000
+                    });
+
+                    // affiche les infos de la station au clique
+                    marker.addListener('click', function () {
+
+                        console.log('ok');
+                        if (window.matchMedia("(max-width: 700px)").matches) {
+                            // Mobile
+                            document.getElementById('map').style.width = 100 + "%";
+                        } else {
+                            // ordinateur
+                            document.getElementById('map').style.width = 65 + "%";
+                        }
+
+                    });
+            });
+                    // position pour groupe de clusters
+                    locations.push({
+                        lat: object.position.lat,
+                        lng: object.position.lng,
+                    });
+
+
+        });
+
+
+        // Ajoute les groupes clusters dans la map.
+        var markers = locations.map(function (location, i) {
+            return new google.maps.Marker({
+                position: location,
+
+            });
+
+        });
+        var markerCluster = new MarkerClusterer(map, markers, {
+            imagePath: 'http://localhost/parcour-5/web/images/m'
+        });
+
+
+    });
+
+
+}
+
+var locations = []
